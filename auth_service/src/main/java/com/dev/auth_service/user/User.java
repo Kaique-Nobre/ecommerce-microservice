59dc3069edef06814058340b71a3edbc62cda38c
Kaique <kaiquenobre71@gmail.com>
2026-09-04 15:45:23 -0300

    implementação da entidade user e UserRepository

---
+++ auth_service/src/main/java/com/dev/auth_service/user/User.java
@@ -0,0 +1,50 @@
+package com.dev.auth_service.user;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+    @Id
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private Role role;
+
+    @Column(nullable = false)
+    private OffsetDateTime createdAt;
+
+    public static User create(String name, String email, String hashedPassword) {
+        User user = new User();
+
+        user.id = UUID.randomUUID();
+        user.name = name;
+        user.email = email;
+        user.password = hashedPassword;
+        user.role = Role.ROLE_USER;
+        user.createdAt = OffsetDateTime.now();
+
+        return user;
+    }
+}
